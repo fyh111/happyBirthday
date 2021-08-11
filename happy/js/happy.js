35ba40c261096254aa89e106bc272463cdbb6c25
fanyuhua <fanyuhua@360.cn>
2021-08-11 17:59:05 +0800

    change

--- happy/js/happy.js
+++ happy/js/happy.js
@@ -3,22 +3,43 @@ const clientWidth = document.documentElement.clientWidth || document.body.client
 const mutech = clientHeight / 10;
 const mutecw = clientWidth / 10;
 
-var initNum = 0;
-var initElementArr = ['.initPage', '.firstPage', '.secondPage', 'thirdPage'];
+const initElementArr = ['.initPage', '.firstPage', '.secondPage', 'thirdPage'];
+const text = ['哈哈', '嘻嘻', '嘿嘿', '哈哈哈'];
 
-const initPage = document.querySelector('.initPage');
+const initPart = document.querySelector('.initPart');
 const starPart = document.querySelector('.starPart');
 const metePart = document.querySelector('.meteorPart');
 const boomPart = document.querySelector('.boomPart');
 
+var initNum = 0;
+var clickNum = 0;
+
+var timerMeteor = null;
 var timerBoom = null;
 // 开始
 window.onload = function () {
   document.addEventListener('click', function (event) {
     console.log(event.clientX, event.clientY);
+    let showText = text[clickNum];
+    clickNum = clickNum === text.length - 1 ? 0 : clickNum + 1;
+    let tagP = document.createElement('p');
+    tagP.classList.add('helloTip');
+    document.body.appendChild(tagP);
+    $(tagP).text(showText).css({
+      top: event.clientY - 20 + 'px',
+      left: event.clientX + 'px',
+      color: getRandomColor()
+    }).stop().animate({
+      top: event.clientY - 50 + 'px',
+      opacity: 0
+    }, 1000, function () {
+      document.body.removeChild(tagP);
+    })
   }, false)
-  document.addEventListener('mousewheel', function(event){
-    debounce(mousewheelEvent, 500, event);
+  document.addEventListener('mousewheel', function (event) {
+    if (!event.ctrlKey) {
+      debounce(mousewheelEvent, 500, event);
+    }
   }, false);
 }
 
@@ -42,6 +63,13 @@ function getRandomY(num) {
 // 获得min和max之间的随机数
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+// 获得随机颜色
+function getRandomColor() {
+  let r = getRandom(0,255);
+  let g = getRandom(0,255);
+  let b = getRandom(0,255);
+  return `rgb(${r}, ${g}, ${b})`;
 }
 // 函数节流,一定时间内只能触发一次
 const trotol = (() => {
@@ -106,18 +134,60 @@ function createStar() {
       starPart.appendChild(div);
     }
   }
-  createStar = function () {};
+  // createStar = function () {};
 }
 // 第二部分:流星-------------------------------------------------------------------------------------
-function meteor() {
-  let x = getRandomX(clientWidth);
-  let y = clientHeight;
-  let div = document.createElement('div');
-  div.classList.add('meteor');
-  metePart.appendChild(div);
-  $(div).css({
+function createMeteor(num) {
+  let muteX = Math.floor(clientWidth / num);
+  for (let i = 0; i < num; i++) {
+    let startX = Math.floor(getRandom(muteX * i, muteX * (i + 1)));
+    let startY = clientHeight;
+    let endY = Math.floor(getRandom(0, clientHeight / 2));
+    let endX = startX - (startY - endY);
+    setTimeout(() => {
+      meteor(startX, startY, endX, endY)
+    }, getRandom(0, 5000));
+  }
+}
+
+function meteor(x, y, ex, ey) {
+  let hght = (y - ey) * 1.5;
+  let meteorPath = document.createElement('div');
+  meteorPath.classList.add('meteorPath');
+  metePart.appendChild(meteorPath);
+  $(meteorPath).css({
+    height: hght + 'px',
     left: x + 'px'
   })
+  let div = document.createElement('div');
+  div.classList.add('meteor');
+  meteorPath.appendChild(div);
+  // 设置流星的起止点
+  $(div).stop().animate({
+    top: hght - 5 + 'px'
+  }, getRandom(7000, 9000), 'linear', function () {
+    meteorPath.removeChild(div);
+  })
+  // 设置流星轨迹
+  let timer = setInterval(() => {
+    let divCopy = document.createElement('div');
+    divCopy.classList.add('meteorCopy');
+    meteorPath.appendChild(divCopy);
+    $(divCopy).css({
+      top: $(div).css('top'),
+      left: $(div).css('left')
+    }).stop().animate({
+      opacity: 0
+    }, 2000, 'linear', function () {
+      meteorPath.removeChild(divCopy);
+    })
+    if ($(div).is(':hidden')) {
+      clearInterval(timer);
+      setTimeout(() => {
+        metePart.removeChild(meteorPath);
+      }, 2000);
+    }
+  }, 20);
 }
 // 第三部分:烟花-------------------------------------------------------------------------------------
 // 烟花爆炸后的随机路径
@@ -133,11 +203,13 @@ function startBoom(num) {
     let startPointX = clientWidth / 2;
     let endPointX = Math.floor(getRandomX(clientWidth));
     let endPointY = Math.floor(getRandomY(clientHeight));
-    startAndEnd(startPointX, 0, endPointX, endPointY);
+    setTimeout(() => {
+      boomStartAndEnd(startPointX, 0, endPointX, endPointY);
+    }, getRandom(0, 4000));
   }
 }
 // 烟花的起止点
-function startAndEnd(startX, startY, endX, endY) {
+function boomStartAndEnd(startX, startY, endX, endY) {
   let k = (endY - startY) / (endX - startX);
   let angleInit = Math.atan2(endY, endX - startX) * 180 / Math.PI;
   let angle = k > 0 ? 90 - angleInit : -(angleInit - 90);
@@ -151,7 +223,7 @@ function startAndEnd(startX, startY, endX, endY) {
   }).animate({
     bottom: endY + 'px',
     left: endX + 'px'
-  }, 2000, function () {
+  }, 2000, 'swing', function () {
     boom(div, endX, endY);
   })
 }
@@ -205,51 +277,89 @@ function nextPageClick(num) {
   console.log(num, '------->');
   switch (num) {
     case 0:
-      clearTimeout(timerBoom);
+      clearInterval(timerBoom);
+      clearInterval(timerMeteor);
+      $('.initPage').css({
+        display: 'block'
+      })
       $('.initPage').stop().animate({
         opacity: 1
-      }, 1000, function () {
-        console.log('init');
-      })
+      }, 1000)
       $('.initPage').siblings().stop().animate({
         opacity: 0
-      }, 1000)
+      }, 1000, function () {
+        $('.initPage').siblings().css({
+          display: 'none'
+        })
+      })
       break;
     case 1:
-      clearTimeout(timerBoom);
+      clearInterval(timerBoom);
+      clearInterval(timerMeteor);
+      $('.firstPage').css({
+        display: 'block'
+      })
       $('.firstPage').stop().animate({
         opacity: 1
       }, 1000, function () {
-        createStar();
+        $(starPart).empty();
+        // $(boomPart).empty();
+        createMeteor(4);
+        timerMeteor = setInterval(() => {
+          createMeteor(4);
+        }, getRandom(5000, 8000));
       })
       $('.firstPage').siblings().stop().animate({
         opacity: 0
-      }, 1000)
+      }, 1000, function () {
+        $('.firstPage').siblings().css({
+          // display: 'none'
+        })
+      })
       break;
     case 2:
-      clearTimeout(timerBoom);
+      clearInterval(timerBoom);
+      clearInterval(timerMeteor);
+      $('.secondPage').css({
+        display: 'block'
+      })
       $('.secondPage').stop().animate({
         opacity: 1
-      }, 1000, function() {
-        console.log('second');
+      }, 1000, function () {
+        $(initPart).empty();
+        // $(metePart).empty();
+        createStar();
       })
       $('.secondPage').siblings().stop().animate({
         opacity: 0
-      }, 1000)
+      }, 1000, function () {
+        $('.secondPage').siblings().css({
+          // display: 'none'
+        })
+      })
       break;
     case 3:
-      clearTimeout(timerBoom);
+      clearInterval(timerBoom);
+      clearInterval(timerMeteor);
+      $('.thirdPage').css({
+        display: 'block'
+      })
       $('.thirdPage').stop().animate({
         opacity: 1
       }, 1000, function () {
+        $(starPart).empty();
         startBoom(5);
         timerBoom = setInterval(() => {
           startBoom(5);
-        }, 3000);
+        }, 4000);
       })
       $('.thirdPage').siblings().stop().animate({
         opacity: 0
-      }, 1000)
+      }, 1000, function () {
+        $('.thirdPage').siblings().css({
+          // display: 'none'
+        })
+      })
       break;
   }
 }
